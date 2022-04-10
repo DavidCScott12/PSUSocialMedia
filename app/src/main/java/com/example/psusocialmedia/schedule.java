666482db93b2506df2b7a4ec15a2c10eaf9f342c
DavidCScott12 <davidcscott5676@gmail.com>
2022-04-10 08:47:09 -0400

    4/9/2022 8:38pm

--- app/src/main/java/com/example/psusocialmedia/schedule.java
+++ app/src/main/java/com/example/psusocialmedia/schedule.java
@@ -4,26 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.ListResult;
 import com.google.firebase.storage.StorageReference;
 
 import java.nio.charset.Charset;
-
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 
 public class schedule extends AppCompatActivity {
     FirebaseStorage storage;
     StorageReference storageRef;
     FirebaseUser user;
-    String[][] allClasses;
+    String[] allClasses;
     final long ONE_MEGABYTE = 1024 * 1024;
     int count;
     int count2;
+    ArrayAdapter<String> arrayAdapter;
+    String[]  dataset;
+    ListView dynamicclasses;
 
     private static String[] convertToStrings(byte[][] byteStrings) {
         String[] data = new String[byteStrings.length];
@@ -31,6 +41,7 @@ public class schedule extends AppCompatActivity {
             data[i] = new String(byteStrings[i], Charset.defaultCharset());
 
         }
+        Log.d("convertToStrings: ", String.valueOf(data));
         return data;
     }
 
@@ -40,37 +51,61 @@ public class schedule extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
 
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReference();
         StorageReference userRef = storageRef.child("classes/"+user.getEmail());
-        count2 = 0;
+        count = 0;
+        allClasses = new String[20];
         userRef.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
             @Override
             public void onSuccess(ListResult listResult) {
-                for (StorageReference item : listResult.getPrefixes())
+                Log.d("schedule:","Success!1");
+                for (StorageReference thing : listResult.getPrefixes())
                 {
-                    byte[][] temp = new byte[4][64];
-                    item.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    thing.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
                         @Override
                         public void onSuccess(ListResult listResult) {
-                            count = 0;
+                            Log.d("schedule:","Success!2");
+
                             for (StorageReference item : listResult.getItems()){
                                 item.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
                                     @Override
                                     public void onSuccess(byte[] bytes) {
-                                        temp[count] = bytes;
+                                        String s =  new String(bytes, StandardCharsets.UTF_8);
+                                        allClasses[count] = s;
+                                        Log.d("schedule:",allClasses[count]);
                                         count++;
                                     }
                                 });
+                                Log.d("schedule1:",allClasses[count]);
                             }
                         }
                     });
-                    allClasses[count2] = convertToStrings(temp);
-                    count2++;
+                    //Log.d("schedule1:",allClasses[count]);
                 }//for
             }//onSuccess
         });//OnSuccsessListener
-        for(int x = 0; x < allClasses.length;x++)
-        Log.d("Classes", allClasses[x].toString());
+        Log.d("stuff", String.valueOf(allClasses[0]));
+        dataset = new String[allClasses.length];
+        for(int i = 1; i < allClasses.length/4; i++){
+            //dataset[i] = "Subject: "+allClasses[i]+"\nClass: "+allClasses[i]+"\nSection: "+allClasses[i];
+        }
+        dynamicclasses = findViewById(R.id.dynamicclasses);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataset);
+        dynamicclasses.setAdapter(arrayAdapter);
+        dynamicclasses.setOnItemClickListener(MshowforItem);
     }//onCreate
+    private AdapterView.OnItemClickListener MshowforItem = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            TextView tvTemp = ((TextView) view);
+
+            tvTemp.setText(dataset[position]);
+
+            arrayAdapter.notifyDataSetChanged();
+        }
+    };
 }//Class
